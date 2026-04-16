68c36987ed2de60a8018827232f59680146a8692
makieman <markmaranga9@gmail.com>
2026-04-16 16:14:14 +0300

    feat: implement responsive footer component and global WhatsApp floating action button

--- Front-end/src/app/shared/components/footer/footer.component.ts
+++ Front-end/src/app/shared/components/footer/footer.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { CartProductsCountService } from '@app/core/services/cart-products-count.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    CommonModule
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  cartCount: number = 0;
 
+  constructor(
+    private productsCount: CartProductsCountService,
+    private zone: NgZone,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    this.productsCount.data$.subscribe({
+      next: (data) => {
+        this.zone.run(() => {
+          this.cartCount = data;
+          this.cdr.markForCheck();
+        });
+      }
+    });
+  }
 }
